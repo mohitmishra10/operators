48838487e4a43876add894319e8dce950c65db6b
Mohit Mishra <136458158+mohitmishra10@users.noreply.github.com>
2023-06-16 15:58:07 +0530

    Add files via upload

---
+++ operator_1.py
@@ -0,0 +1,69 @@
+a=23
+b=15
+#ARITHMETIC OPERATOR
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("floor division:",a//b)
+print("Exponentation:",a**b)
+
+#COMPARISON OPERATOR
+
+print("<=",a<=b)
+print(">=",a>=b)
+print("==",a==b)
+print(">",a>b)
+print("<",a<b)
+print("!=",a!=b)
+
+#MEMBERSHIP OPERATOR
+
+a="mohit mishra"
+print("mohit"in a)
+print("ram"not in a)
+
+#IDENTITY OPERATOR
+
+print("is",b is a)
+print("is not",b is not a)
+
+#LOGICAL OPERATOR
+
+a=True
+b=False
+print("and",a and b)
+print("or",a or b)
+print("not",not b)
+
+#BITWISE OPERATOR
+
+print("bitwise >>",a>>b)
+print("bitwise <<",a<<b)
+print("bitwise &",a&b)
+print("bitwise |",a|b)
+print("bitwise ^",a^b)
+
+#ASSIGNMENT OPERATOR
+
+a+=2
+print("+=",a)
+a=15
+a-=1
+print("-=",a)
+a=23
+a*=1
+print("*=",a)
+a=23
+a/=1
+print("/=",a)
+a=23
+a//=1
+print("//=",a)
+a=23
+a%=1
+print("%=",a)
+a=23
+a**=1
+print("**=",a)
